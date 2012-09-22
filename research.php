7b2abb52bc7624e1a82f01afcf90890ab5b3ce88
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-09-22 12:53:51 -0400

    Add MRFy to research.

--- research.php
+++ research.php
@@ -5,4 +5,7 @@
 <p><a href="http://bcb.cs.tufts.edu/genecentric/">Genecentric: A package to
    uncover graph-theoretic structure in high-throughput epistasis data.</a></p>
 
+<p><a href="http://mrfy.cs.tufts.edu/">MRFy is in-development Haskell software 
+for protein remote homology prediction</a></p>
+
 <?php require_once('homepage_footer.php'); ?>
